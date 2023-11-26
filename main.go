6ce7bbfd70d0c122195513280022d4b409e43608
Ethea2 <nathandres2001@gmail.com>
2023-11-26 11:55:49 +0800

    Added: som logs

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 
 	"github.com/Ethea2/nat-dev/api/route"
 	"github.com/Ethea2/nat-dev/database"
 )
 
 func main() {
+	path, oerr := os.Getwd()
+	if oerr != nil {
+		log.Println(oerr)
+	}
+	fmt.Println(path)
+
+	p, perr := os.Executable()
+	if perr != nil {
+		log.Fatal(perr)
+	}
+
+	godotenv.Load(p, ".env")
+
 	app := fiber.New()
 
 	app.Use(cors.New())
