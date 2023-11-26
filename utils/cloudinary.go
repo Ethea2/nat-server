5a5d7f1de6dc9aafd826bd466383294f26060779
Ethea2 <nathandres2001@gmail.com>
2023-11-26 14:11:16 +0800

    added: removed .env

--- utils/cloudinary.go
+++ utils/cloudinary.go
@@ -14,7 +14,7 @@ import (
 func UploadPhoto(input interface{}) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	godotenv.Load(".env")
+	godotenv.Load()
 
 	cld, err := cloudinary.NewFromParams(
 		os.Getenv("CLOUD_NAME"),
